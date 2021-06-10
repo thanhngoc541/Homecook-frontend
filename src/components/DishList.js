ae366ffc4f5b8452c7ec251009eea285fad18352
dangtrannam <dangtrannam2001@gmail.com>
2021-06-10 17:08:36 +0700

    Edit fetch api

--- src/components/DishList.js
+++ src/components/DishList.js
@@ -11,7 +11,7 @@ import {
 import { Fade, Stagger } from "react-animation-components";
 
 const DishList = () => {
-  const url = "/api/hello";
+  const url = "http://localhost:8080/Homecook_war_exploded/MainServlet";
 
   const [dishes, setDishes] = useState([]);
   const [loading, setLoading] = useState(true);
