c7ef448ccc25298d7a7207dcc66e406e59555760
dangtrannam <dangtrannam2001@gmail.com>
2021-07-12 08:06:20 +0700

    Move: alert dish added to context.js

--- src/components/items/Dish.js
+++ src/components/items/Dish.js
@@ -21,23 +21,6 @@ const Dish = ({ dish, MenuID, key }) => {
       });
       return;
     } else {
-      const Toast = Swal.mixin({
-        toast: true,
-        position: "top-end",
-        showConfirmButton: false,
-        timer: 1500,
-        timerProgressBar: true,
-        didOpen: (toast) => {
-          toast.addEventListener("mouseenter", Swal.stopTimer);
-          toast.addEventListener("mouseleave", Swal.resumeTimer);
-        },
-      });
-
-      Toast.fire({
-        icon: "success",
-        title: "Your dish has been added!",
-      });
-
       addToCart(e, dish);
     }
   };
