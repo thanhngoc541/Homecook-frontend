7f90dfba9aa0012669d3e26294020ed3aef9cc55
dangtrannam <dangtrannam2001@gmail.com>
2021-07-16 20:09:26 +0700

    Update: Cart component

--- src/components/items/Cart.js
+++ src/components/items/Cart.js
@@ -18,10 +18,15 @@ function Cart(props) {
 
   if (cart.length === 0) {
     return (
-      <section
-        className={`${isCartOpen ? "cart-overlay show-cart" : "cart-overlay "}`}
-      >
-        <div className="cart empty-cart">
+      <>
+        {isCartOpen && (
+          <section className="bg-mask" onClick={closeCart}></section>
+        )}
+        <section
+          className={`${
+            isCartOpen ? " cart empty-cart show-cart" : "cart empty-cart"
+          }`}
+        >
           {/* cart header */}
           <button className="close-btn" onClick={closeCart}>
             <i className="fa fa-times-circle" aria-hidden="true"></i>
@@ -30,48 +35,49 @@ function Cart(props) {
             <h2>your bag</h2>
             <h4 className="empty-cart-text">is currently empty</h4>
           </header>
-        </div>
-      </section>
+        </section>
+      </>
     );
   }
 
   return (
     <>
+      {isCartOpen && <section className="bg-mask"></section>}
       <section
-        className={`${isCartOpen ? "cart-overlay show-cart" : "cart-overlay "}`}
+        className={`${
+          isCartOpen ? " cart empty-cart show-cart" : "cart empty-cart"
+        }`}
       >
-        <div className="cart">
-          {/* cart header */}
-          <button className="close-btn" onClick={closeCart}>
-            <i className="fa fa-times-circle" aria-hidden="true"></i>
-          </button>
-          <header className="cart-header">
-            <h3>Your Cart</h3>
-          </header>
-          {/* cart items */}
-          <div className="cart-items">
-            {cart.map((item) => {
-              return <CartItem key={item.id} {...item} />;
-            })}
-          </div>
-          {/* cart footer */}
-          <footer>
-            <hr />
-            <div className="cart-total">
-              <h4>
-                total <span>${total}</span>
-              </h4>
-            </div>
-
-            <button className="btn clear-btn mr-3" onClick={handleCheckout}>
-              Checkout
-            </button>
-
-            <button className="btn clear-btn" onClick={clearCart}>
-              Clear cart
-            </button>
-          </footer>
+        {/* cart header */}
+        <button className="close-btn" onClick={closeCart}>
+          <i className="fa fa-times-circle" aria-hidden="true"></i>
+        </button>
+        <header className="cart-header">
+          <h3>Your Cart</h3>
+        </header>
+        {/* cart items */}
+        <div className="cart-items">
+          {cart.map((item) => {
+            return <CartItem key={item.id} {...item} />;
+          })}
         </div>
+        {/* cart footer */}
+        <footer>
+          <hr />
+          <div className="cart-total">
+            <h4>
+              total <span>${total}</span>
+            </h4>
+          </div>
+
+          <button className="btn clear-btn mr-3" onClick={handleCheckout}>
+            Checkout
+          </button>
+
+          <button className="btn clear-btn" onClick={clearCart}>
+            Clear cart
+          </button>
+        </footer>
       </section>
     </>
   );
